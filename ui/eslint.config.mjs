7d17a12e45da36f5e184d583e6480888eed46602
Tesha Richardson <198785486+lrichardson-flexion@users.noreply.github.com>
2025-03-04 15:10:47 -0800

    Install eslint to frontend ui

---
+++ ui/eslint.config.mjs
@@ -0,0 +1,23 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import pluginReact from "eslint-plugin-react";
+
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {files: ["**/*.{js,mjs,cjs,jsx}"]},
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,{
+    rules: {
+      "no-unused-vars": "warn",
+      "no-undef": "warn"
+  }
+  },
+  pluginReact.configs.flat.recommended,
+  {
+    rules: {
+      'react/react-in-jsx-scope': 0,
+      'react/jsx-uses-react': 0
+    }
+  }
+];
