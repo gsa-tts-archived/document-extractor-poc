f93dcbf2b629b9674d3094318077844441299463
halprin <halprin@users.noreply.github.com>
2025-03-03 19:19:55 -0700

    New Python project format

---
+++ src/build.py
@@ -0,0 +1,41 @@
+# uv run build.py
+
+import subprocess
+from pathlib import Path
+import shutil
+import os
+
+
+def execute(program, *args):
+    subprocess.call([program, *args])
+
+
+def uv(*args):
+    execute("uv", *args)
+
+
+def cp(*args):
+    execute("cp", *args)
+
+
+def build():
+    dist_folder = Path("dist")
+    # build_dir = Path(__file__).parent.joinpath("dist", "build")
+    build_dir = dist_folder.joinpath("build")
+    build_dir.mkdir(parents=True, exist_ok=True)
+
+    uv("export", "--frozen", "--no-dev", "--no-editable", "-o", "requirements.txt")
+    uv("pip", "install", "--no-installer-metadata", "--no-compile-bytecode", "--target", build_dir.as_posix(), "-r", "requirements.txt")
+
+    our_code_path = "src"
+    shutil.copytree(
+        src=our_code_path,
+        dst=build_dir.joinpath(our_code_path).as_posix(),
+        dirs_exist_ok=True
+    )
+
+    shutil.make_archive(dist_folder.joinpath("lambda"), "zip", build_dir)
+
+
+if __name__ == "__main__":
+    build()
