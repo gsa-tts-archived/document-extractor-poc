118524b964d2bb684b14b3396049aaa0f6a57ab1
halprin <halprin@users.noreply.github.com>
2025-03-18 15:40:39 -0600

    Add the other W2 queries

--- backend/ocr.py
+++ backend/ocr.py
@@ -1,8 +1,9 @@
 from src.external.ocr.textract import Textract
+from src.forms import w2
 
 if __name__ == "__main__":
     scanner = Textract()
-    result = scanner.scan("s3://document-extractor-gsa-dev-documents/test_ws.jpg", queries=None)
+    result = scanner.scan("s3://document-extractor-gsa-dev-documents/test_ws.jpg", queries=w2.queries)
 
     for key, value in result.items():
         print(key)
