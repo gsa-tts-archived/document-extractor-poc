95119dc3e3bf2a898f0921448185de10dc1d3796
halprin <halprin@users.noreply.github.com>
2025-03-21 15:56:32 -0600

    instantiate forms automatically

--- backend/src/forms/__init__.py
+++ backend/src/forms/__init__.py
@@ -15,10 +15,11 @@ def find_form_implementations():
         module = importlib.import_module(f"{__name__}.{module_name}")
 
         # Iterate over all classes in the module
-        for _, obj in inspect.getmembers(module, inspect.isclass):
+        for _, clazz in inspect.getmembers(module, inspect.isclass):
             # Check if the class is a subclass of the base class and is not the base class itself
-            if issubclass(obj, Form) and obj is not Form:
-                implementations.append(obj)
+            if issubclass(clazz, Form) and clazz is not Form:
+                instantiation = clazz()
+                implementations.append(instantiation)
 
     return implementations
 
