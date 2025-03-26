80d0148c4a84700ae7e96b159296acf5bffdfb11
halprin <halprin@users.noreply.github.com>
2025-03-26 13:28:44 -0600

    Update 1099-NEC extraction for what we need

--- backend/src/forms/ten_ninety_nine_nec.py
+++ backend/src/forms/ten_ninety_nine_nec.py
@@ -10,7 +10,6 @@ class TenNinetyNineNec(Form):
 
     def queries(self) -> list[str]:
         return [
-            "For calendar year",
             "PAYER'S name, street address, city or town, state or province, country, ZIP or foreign postal code, and "
             "telephone no.",
             "PAYER'S TIN",
@@ -20,7 +19,6 @@ class TenNinetyNineNec(Form):
             "RECIPIENT's city or town, state or province, country, and ZIP or foreign postal code",
             "Account number",
             "1 Nonemployee compensation",
-            "2 Payer made direct sales totalling $5,000 or more of consumer products to recipient for resale",
             "3",
             "4 Federal income tax withheld",
             "4 State tax withheld",
