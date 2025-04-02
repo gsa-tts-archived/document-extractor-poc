c1142351b1ff9aefd42b72467cf7aea564d1f4e0
saquino0827 <saquino@flexion.us>
2025-04-02 12:13:51 -0500

    Update unit tests for test_upload_document.test_generating_secure_filename_works - to better represent asserting of uuid by using it's library to ensure it is a valid uuid string

--- backend/tests/documents/test_upload_document.py
+++ backend/tests/documents/test_upload_document.py
@@ -1,4 +1,5 @@
 import base64
+import uuid
 from unittest import mock
 
 import pytest
@@ -35,6 +36,7 @@ def test_uploading_file_data_returns_document_id():
     response = upload_file_data("original.txt", decoded_file_content, "mock_bucket", "mock_folder")
 
     assert response is not None
+    assert isinstance(response, str)
 
 
 def test_generating_secure_filename_works():
@@ -42,21 +44,22 @@ def test_generating_secure_filename_works():
 
     expected_secure_file_name, expected_document_id = generate_secure_filename(mock_original_filename)
 
-    # added coverage for asserts but I need to address how to assert these values properly
-    assert mock_original_filename is not None
+    assert expected_secure_file_name is not None
     assert expected_document_id is not None
+    assert expected_secure_file_name.removesuffix(".txt") == expected_document_id
+    assert str(uuid.UUID(expected_document_id)) == expected_document_id
 
 
-def test_generating_file_data_with_generate_file_id():
+def test_decode_file_content_provides_valid_output():
     mock_file_content = b"Hello, this is a test file."
     decoded_content = base64.b64encode(mock_file_content).decode("utf-8")
 
     response = decode_file_content(decoded_content)
 
-    assert response is not None
+    assert response == mock_file_content
 
 
-def test_generating_file_data_with_invalid_file_content():
+def test_decode_file_content_with_bad_type():
     mock_file_content = 1234
     with pytest.raises(TypeError):
         decode_file_content(mock_file_content)
