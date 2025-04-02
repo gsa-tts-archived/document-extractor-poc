926b7f144b1295a8cf05b7251c1762ffd69f0cd4
halprin <halprin@users.noreply.github.com>
2025-04-02 16:33:21 -0600

    More tests for dynamodb helper methods

--- backend/tests/external/aws/test_dynamodb.py
+++ backend/tests/external/aws/test_dynamodb.py
@@ -67,3 +67,69 @@ def test_convert_from_decimal_complex():
         },
         "d": "Something else",
     }
+
+
+def test_convert_to_decimal_direct_decimal():
+    output = DynamoDb._convert_to_decimal(1.2)
+    assert output == Decimal("1.2")
+
+
+def test_convert_to_decimal_list():
+    test_input = [1.2, 2.3, 3.4]
+    output = DynamoDb._convert_to_decimal(test_input)
+    assert output == [Decimal("1.2"), Decimal("2.3"), Decimal("3.4")]
+
+
+def test_convert_to_decimal_dict():
+    test_input = {
+        "a": 1.2,
+        "b": 2.3,
+        "c": 3.4,
+    }
+    output = DynamoDb._convert_to_decimal(test_input)
+    assert output == {
+        "a": Decimal("1.2"),
+        "b": Decimal("2.3"),
+        "c": Decimal("3.4"),
+    }
+
+
+def test_convert_to_decimal_complex():
+    test_input = {
+        "a": 1.2,
+        "b": [
+            2.3,
+            3.4,
+            {
+                "1": 3.4,
+                "2": 2.3,
+            },
+        ],
+        "c": {
+            "1": 1.2,
+            "2": 2.3,
+            "3": [
+                3.4,
+                2.3,
+            ],
+        },
+        "d": "Something else",
+    }
+    output = DynamoDb._convert_to_decimal(test_input)
+    assert output == {
+        "a": Decimal("1.2"),
+        "b": [
+            Decimal("2.3"),
+            Decimal("3.4"),
+            {
+                "1": Decimal("3.4"),
+                "2": Decimal("2.3"),
+            },
+        ],
+        "c": {
+            "1": Decimal("1.2"),
+            "2": Decimal("2.3"),
+            "3": [Decimal("3.4"), Decimal("2.3")],
+        },
+        "d": "Something else",
+    }
