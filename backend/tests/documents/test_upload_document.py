ba0c6087fe262a40a9eed090d9411c5a2c910035
saquino0827 <saquino@flexion.us>
2025-04-02 15:01:34 -0500

    Refactor tests to be snazzier

--- backend/tests/documents/test_upload_document.py
+++ backend/tests/documents/test_upload_document.py
@@ -32,30 +32,31 @@ def test_uploading_file_data_returns_document_id():
     mock_cloud_storage.put_object.return_value = None
     context.register(CloudStorage, mock_cloud_storage)
 
-    response = upload_file_data("original.txt", decoded_file_content, "mock_bucket", "mock_folder")
+    actual_document_id = upload_file_data("original.txt", decoded_file_content, "mock_bucket", "mock_folder")
 
-    assert response is not None
-    assert isinstance(response, str)
+    assert actual_document_id is not None
+    assert isinstance(actual_document_id, str)
+    assert str(uuid.UUID(actual_document_id)) == actual_document_id
 
 
 def test_generating_secure_filename_works():
     mock_original_filename = "Original.txt"
 
-    expected_secure_file_name, expected_document_id = generate_secure_filename(mock_original_filename)
+    actual_secure_file_name, actual_document_id = generate_secure_filename(mock_original_filename)
 
-    assert expected_secure_file_name is not None
-    assert expected_document_id is not None
-    assert expected_secure_file_name.removesuffix(".txt") == expected_document_id
-    assert str(uuid.UUID(expected_document_id)) == expected_document_id
+    assert actual_secure_file_name is not None
+    assert actual_document_id is not None
+    assert actual_secure_file_name.removesuffix(".txt") == actual_document_id
+    assert str(uuid.UUID(actual_document_id)) == actual_document_id
 
 
 def test_decode_file_content_provides_valid_output():
     mock_file_content = b"Hello, this is a test file."
     decoded_content = base64.b64encode(mock_file_content).decode("utf-8")
 
-    response = decode_file_content(decoded_content)
+    actual_decoded_file = decode_file_content(decoded_content)
 
-    assert response == mock_file_content
+    assert actual_decoded_file == mock_file_content
 
 
 def test_decode_file_content_with_bad_type():
