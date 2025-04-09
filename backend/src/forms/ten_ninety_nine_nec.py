7f9ba7a491b2e8ce68cfa5ff6f65838b6cf9ec30
halprin <halprin@users.noreply.github.com>
2025-04-09 13:18:51 -0600

    Fix 1099-NEC state tas withheld query

--- backend/src/forms/ten_ninety_nine_nec.py
+++ backend/src/forms/ten_ninety_nine_nec.py
@@ -21,7 +21,7 @@ class TenNinetyNineNec(Form):
             "1 Nonemployee compensation",
             "3",
             "4 Federal income tax withheld",
-            "4 State tax withheld",
+            "5 State tax withheld",
             "6 State/Payer's state no.",
             "7 State income",
         ]
