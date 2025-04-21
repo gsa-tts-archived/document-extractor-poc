fc0c30520b0edf79860dd94ae2fbc5e9673b8c56
saquino0827 <saquino@flexion.us>
2025-04-21 09:27:03 -0500

    Update login auth

--- backend/src/external/aws/lambdas/login.py
+++ backend/src/external/aws/lambdas/login.py
@@ -1,10 +1,11 @@
 import datetime
 import json
+import logging
 import os
 
+import bcrypt
 import boto3
 import jwt
-from argon2 import PasswordHasher
 
 # Use AWS secrets
 client = boto3.client("secretsmanager")
@@ -14,21 +15,43 @@ ALGORITHM = "RS512"
 
 
 def lambda_handler(event, context):
-    body = json.loads(event.get("body", "{}"))
-    ph = PasswordHasher()
-    username = body.get("username")
-    password = body.get("password")
-
+    if "body" not in event:
+        return {
+            "statusCode": 400,
+            "body": json.dumps({"error": "No credentials provided"}),
+        }
+    body = json.loads(event["body"])
     # AWS Secret Manager User Credentials
-    stored_username = client.get_secret_value(SecretId=f"document-extractor-{ENVIRONMENT}-username")["SecretString"]
-    stored_password = client.get_secret_value(SecretId=f"document-extractor-{ENVIRONMENT}-password")["SecretString"]
+    try:
+        username = body["username"]
+        password = body["password"]
+
+        stored_username = client.get_secret_value(SecretId=f"document-extractor-{ENVIRONMENT}-username")["SecretString"]
+        stored_password = client.get_secret_value(SecretId=f"document-extractor-{ENVIRONMENT}-password")["SecretString"]
+    except Exception as e:
+        exception_message = "Failed to retrieve the secrets"
+        logging.error(exception_message)
+        logging.exception(e)
+        return {
+            "statusCode": 500,
+            "body": json.dumps(exception_message),
+        }
 
     # AWS Secret Manager to hold one user credential
-    if username != stored_username or ph.verify(stored_password, password):
+    if username != stored_username or not bcrypt.checkpw(password.encode("utf-8"), stored_password.encode("utf-8")):
         return {"statusCode": 401, "body": json.dumps({"error": "Invalid credentials"})}
 
     # Create JWT token
-    payload = {"sub": username, "exp": datetime.datetime.now(datetime.UTC) + datetime.timedelta(hours=1)}
-    token = jwt.encode(payload, SECRET_KEY, algorithm=ALGORITHM)
+    try:
+        payload = {"sub": username, "exp": datetime.datetime.now(datetime.UTC) + datetime.timedelta(hours=1)}
+        token = jwt.encode(payload, SECRET_KEY, algorithm=ALGORITHM)
+    except Exception as e:
+        exception_message = "Failed to generate token"
+        logging.error(exception_message)
+        logging.exception(e)
+        return {
+            "statusCode": 500,
+            "body": json.dumps(exception_message),
+        }
 
     return {"statusCode": 200, "body": json.dumps({"access_token": token, "token_type": "bearer"})}
