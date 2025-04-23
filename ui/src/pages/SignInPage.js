8d206c078945aec512747abfe1098d1efae85946
halprin <halprin@users.noreply.github.com>
2025-04-23 13:51:33 -0600

    Remove banner that user is logged out

--- ui/src/pages/SignInPage.js
+++ ui/src/pages/SignInPage.js
@@ -83,11 +83,6 @@ export default function SignInPage() {
             </div>
           )}
 
-          <div className="usa-alert usa-alert--info">
-            <div className="usa-alert__body">
-              <p className="usa-alert__text">Signed out successfully.</p>
-            </div>
-          </div>
           <h1>Sign in for existing users</h1>
 
           <form onSubmit={handleLogin} id="signin-form">
