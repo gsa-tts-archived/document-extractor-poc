b903b5d2492deb405f017f1a9b90049abdf77bce
Tesha Richardson <198785486+lrichardson-flexion@users.noreply.github.com>
2025-05-01 20:23:50 -0400

    Change h4 to h2 and h3 for proper heading hierarchy on download page

--- ui/src/pages/DownloadPage.js
+++ ui/src/pages/DownloadPage.js
@@ -158,9 +158,9 @@ export default function DownloadPage({ signOut }) {
             <li className="usa-card tablet:grid-col-6 widescreen:grid-col-4">
               <div className="usa-card__container">
                 <div className="usa-card__header">
-                  <h4 className="usa-card__heading font-body-md">
+                  <h2 className="usa-card__heading font-body-md">
                     File download
-                  </h4>
+                  </h2>
                 </div>
                 <div className="usa-card__body">
                   {/* Start radio button section  */}
@@ -223,7 +223,7 @@ export default function DownloadPage({ signOut }) {
         </form>
         <div id="preview-section">
           <h2 id="preview-section-title">File Preview</h2>
-          <h4 id="preview-section-file-name">File name</h4>
+          <h3 id="preview-section-file-name">File name</h3>
           <div>{displayPreviewTable()}</div>
         </div>
       </div>
